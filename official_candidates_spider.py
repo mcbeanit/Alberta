345d3bb6f489f83564f60f19763881a82c15288a
Robert W. McBean <mcbeanit@users.noreply.github.com>
2023-05-04 04:56:17 -0600

    official list of candidates from elections alberta. compare to other lists.

--- official_candidates_spider.py
+++ official_candidates_spider.py
@@ -1,19 +1,37 @@
 import scrapy
 import re
 
-public class OfficialCandidatesSpider(scrapy.Spider):
-    name = officialcandidatesspider
+
+class OfficialCandidatesSpider(scrapy.Spider):
+    name = 'officialcandidatesspider'
+    exp_rows = r'^<table.+?(<tr>(.*?><td.+?<\/td>)<\/tr>)+'
+    # exp_riding = r'^<div.+?id=\"(.+?)\">'  if we need to get the riding id
 
     def start_requests(self):
-        url = {'https://www.elections.ab.ca/current-election-information/candidates/'}
-        yield scrapy.Request(url=url, allback=self.parse, errback=self.on_error)
+        urls = {'https://www.elections.ab.ca/current-election-information/candidates/'}
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse, errback=self.on_error)
 
     def parse(self, response):
-        pass
+        # each riding is a <table>, and the candidates in that riding are <tr>
+        table = response.xpath('//div[@class="content"]/table[@class="has-alt-colors"]').getall()
+        assert table
+        riding = ''
+
+        with open('official_candidates.html', 'wt') as data:
+            data.write(f'riding\tdata\n')
+            for c in table:
+                c = c.replace('\n', '')
+                c = c.replace('\r', '')
+                c = c.replace('\t', '')
+
+                matches = re.match(self.exp_rows, c)
+                if matches is not None:
+                    data.write(f'{riding},{c}\n')
+                else:
+                    data.write('not matched\n')
+
+            data.close()
 
     def on_error(self, failure):
         pass
-
-    
-
-
---
+++ test_official_candidates.py
@@ -0,0 +1,24 @@
+import pytest
+import re
+
+exp_rows = r'^^<table.+?<tbody>(.+)<\/tbody>'
+test_row = '<table class="has-alt-colors"><thead><tr><th>Candidate</th><th>Official Agent</th></tr></thead><tbody><tr><td style="width:33.33%; min-width:150px;">DRUH FARRELL<br>ALBERTA NDP<br><a href="https://www.albertandp.ca/" target="_blank">www.albertandp.ca</a></td><td style="min-width:300px;">Harris Kirshenbaum<br>Phone: <a href="tel:7804742415">780-474-2415</a><br>Address: 201 - 10544 114 STREET NW, EDMONTON T5H3J7</td></tr><tr><td style="width:33.33%; min-width:150px;">DEMETRIOS NICOLAIDES<br>UNITED CONSERVATIVE PARTY<br><a href="https://www.unitedconservative.ca/" target="_blank">www.unitedconservative.ca</a></td><td style="min-width:300px;">Blackett Blackett<br>Phone: <a href="tel:5878850543">587-885-0543</a><br>Address: 125, 6311 BOWNESS RD NW, CALGARY T3B0E4</td></tr></tbody></table>'
+exp_fields = r'^<tr><td\s.+?>(.+?)<br>(.+?)<br>.+?<td\s.+?>(.+?)<br>.+<\/td><\/tr>$'
+
+def test_match_rows():
+    # extact the list of <tr>
+    matched = re.match(exp_rows, test_row)
+    assert matched
+    body = matched[1]
+    assert body
+    body = re.sub('</tr>', '</tr>\n', body)
+    # print(body)
+    body = body.strip().split('\n')
+    count = 0
+    for b in body:
+        count = count + 1
+        matched = re.match(exp_fields, b)
+        if matched is not None:
+            print(f'{matched[1]}\n'.title())
+            print(f'{matched[2]}\n'.title())
+            print(f'{matched[3]}\n'.title())
