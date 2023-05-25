4102354c79eba7bbc08c65c2721dcf21a8dc3179
Robert W. McBean <mcbeanit@users.noreply.github.com>
2023-05-25 17:52:50 -0600

    updated candidate social list add current date to 338 list

--- ab_338_ridings.py
+++ ab_338_ridings.py
@@ -1,4 +1,5 @@
 import re
+from datetime import date
 
 ab_338_html_filename = 'ab_338_ridings.html'
 ab_338_csv_filename = 'ab_338_ridings.csv'
@@ -8,16 +9,19 @@ tr_expr = r'^<tr>.*?<td.+?><a href=\"(https:.+?)\".+?>(.+?)<\/a><\/td><td.+?>(.+
 def parse_riding_polls_html():
     with open(ab_338_html_filename, 'rt') as html, open(ab_338_csv_filename, 'wt') as csvfile:
         count = 0
+        today = date.today()
         for tr in html.readlines():
             matches = re.match(tr_expr, tr)
             if matches:
-                csvfile.write(f'{matches[2]},{matches[1]},{matches[4]}\n')
+                csvfile.write(f'{matches[2]},{matches[1]},{matches[4]}\t{today}\n')
                 count = count + 1
             else:
                 print(f'not matched: {tr}\n')
     # html.close()
     # csvfile.close()
     print(f'ab_338_ridings.py: There were {count} ridings found with polls.')
+
+
 
 
 if __name__ == '__main__':
