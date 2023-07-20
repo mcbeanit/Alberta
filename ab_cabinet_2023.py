fd2ea9a98c50f4f668b752a729cc720fc4b34a67
Robert W. McBean <mcbeanit@users.noreply.github.com>
2023-07-20 04:01:56 -0600

    2023 alberta cabinet list

--- ab_cabinet_2023.py
+++ ab_cabinet_2023.py
@@ -1,18 +1,19 @@
 import re
 import csv
 
+flds_expr = r'^<tr><td>(.+?)<\/td><td>(.+?)<\/td><td>(.+?)<\/td><\/tr>$'
+
+
 def parse_party_html(htmlfile='ab_cabinet_2023.html', csvfile='ab_cabinet_2023.csv'):
     with open(htmlfile, "rt") as h, open(csvfile, "wt") as csv:
         count = 0
-        for p in h.readlines():
+        for tr in h.readlines():
             count = count + 1
-            
-
-
+            matches = re.match(flds_expr, tr)
+            if matches:
+                print(f'{matches[1]},{matches[2]},{matches[3]}\n')
 
 
 
 if __name__ == '__main__':
     parse_party_html()
-
-
--- ab_cabinet_2023_spider.py
+++ ab_cabinet_2023_spider.py
@@ -4,24 +4,26 @@ import re
 class ABCabinet2023Spider(scrapy.Spider):
     name = 'ABCabinet2023Spider'
     html_file = 'ab_cabinet_2023.html'
-    flds_expr = '^<tr><td>(.+?)<\/td><td>(.+?)<\/td><td>(.+?)<\/td><\/tr>$'
+
 
     def start_requests(self):
         url = 'https://en.wikipedia.org/wiki/Executive_Council_of_Alberta'
         yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response, **kwargs):
-        trs = response.xpath('//table[@class="wikitable"]/tr').getall()
-        with open (html_file) as html:
+        trs = response.xpath('//table[@class="wikitable"]/tbody/tr').getall()
+        with open (self.html_file, 'wt') as html:
             count = 0
             # ignore the first line which is a row header
+            # print (trs)
             for tr in trs:
                 count = count + 1
                 if count > 1:
                     tr = tr.replace('\n', '')
                     tr = tr.replace('\r', '')
                     tr = tr.replace('\t', '')
-                    html.write(f'{tr}\n')        
+                    html.write(f'{tr}\n')
+        html.close()
 
 
 
